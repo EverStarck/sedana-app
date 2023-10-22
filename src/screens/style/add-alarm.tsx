c31189c2d5b5aa179b8e19b52e69802e1c19b61b
Ever <everstarckone@protonmail.com>
2023-10-22 15:01:56 -0600

    fix: add alarm time closes every keystroke

--- src/screens/style/add-alarm.tsx
+++ src/screens/style/add-alarm.tsx
@@ -14,7 +14,6 @@ import {
 import { Title } from './title';
 import type { Option } from '@/ui';
 import useAlarmStore from '@/core/alarms';
-import uuid from 'uuid-random';
 
 const schema = z.object({
   name: z.string().min(10),
@@ -50,27 +49,35 @@ export const AddAlarm = () => {
   const [rangeList, setRangeList] = React.useState<
     Array<{ range: string | null; time: number | null }>
   >([{ range: null, time: null }]);
+  const [numInputs, setNumInputs] = React.useState(1);
+  const refInputs = React.useRef<string[]>([]);
 
   const addNewRange = () => {
     setRangeList([...rangeList, { range: null, time: null }]);
+    refInputs.current.push('');
+    setNumInputs(numInputs + 1);
+  };
+
+  const setInputValue = (index: number, value: string) => {
+    const re = /^[0-9\b]+$/;
+    if (value !== '' && !re.test(value)) {
+      return;
+    }
+
+    refInputs.current[index] = value;
+
+    const newRangeList = [...rangeList];
+    newRangeList[index].time = Number(value);
+    setRangeList(newRangeList);
   };
 
   const onSubmit = (data: FormType) => {
-    console.log('🚀 ~ file: add-alarm.tsx:52 ~ onSubmit ~ data:', data);
-    console.log(
-      '🚀 ~ file: add-alarm.tsx:59 ~ onSubmit ~ rangeList:',
-      rangeList
-    );
     if (data.name?.length < 4) {
       showErrorMessage('Alarm Name must be at least 4 characters');
       return;
     }
 
     const cleanedRanges = cleanData(rangeList);
-    console.log(
-      '🚀 ~ file: add-alarm.tsx:59 ~ onSubmit ~ cleanedRanges:',
-      cleanedRanges
-    );
 
     if (!cleanedRanges.length) {
       showErrorMessage(
@@ -90,58 +97,40 @@ export const AddAlarm = () => {
     });
   };
 
+  const inputs = [];
+  for (let i = 0; i < numInputs; i++) {
+    inputs.push(
+      <View key={i}>
+        <Title text={`${i + 1}`} />
+        <Select
+          label="Select"
+          options={options}
+          placeholder="Select"
+          value={rangeList[i]?.range || undefined}
+          onSelect={(option) => {
+            const newRangeList = [...rangeList];
+            newRangeList[i].range = option as string;
+            setRangeList(newRangeList);
+          }}
+        />
+        <Input
+          label="Time to eat (seconds)"
+          placeholder="60"
+          keyboardType="numeric"
+          value={refInputs.current[i] === undefined ? '' : refInputs.current[i]}
+          onChangeText={(text) => setInputValue(i, text)}
+        />
+      </View>
+    );
+  }
+
   return (
     <ScrollView>
       <View className="flex-1 p-4 ">
         <ControlledInput name="name" label="Alarm Name" control={control} />
 
         <Title text="Ranges" />
-        {rangeList.map((rangeItem, index) => {
-          return (
-            <View key={uuid()}>
-              <Title text={`${index + 1}`} />
-              <Select
-                label="Select"
-                options={options}
-                placeholder="Select"
-                value={rangeItem.range || undefined}
-                onSelect={(option) => {
-                  const newRangeList = [...rangeList];
-                  newRangeList[index].range = option as string;
-                  setRangeList(newRangeList);
-                }}
-              />
-              <Input
-                label="Time to eat (seconds)"
-                placeholder="60"
-                keyboardType="numeric"
-                value={
-                  isNaN(Number(rangeItem.time))
-                    ? ''
-                    : rangeItem.time?.toString()
-                }
-                onChangeText={(text) => {
-                  const newRangeList = [...rangeList];
-                  newRangeList[index].time = Number(text);
-                  setRangeList(newRangeList);
-                }}
-              />
-
-              {/* <ControlledSelect
-              name={`range${index}`}
-              label="Select"
-              control={control}
-              options={options}
-            />
-            <ControlledInput
-              name={`time${index}`}
-              label="Time to eat (seconds)"
-              control={control}
-              keyboardType="numeric"
-            /> */}
-            </View>
-          );
-        })}
+        {inputs}
 
         {rangeList.length < options.length && (
           <Button
